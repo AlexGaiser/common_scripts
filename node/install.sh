4710de3533f0e832ba605be81ef9052d9eb807cc
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 16:30:03 -0400

    update install script, remove tar file

--- node/install.sh
+++ node/install.sh
@@ -4,9 +4,9 @@ branch="main"
 path="node/bundle"
 file="scripts.tar.gz"
 
-url="https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path}"
-
-echo $url
-curl -L -O https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path}/{$file}
-
+url=https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path}/${file}
+echo "downloading from: $url"
+curl -L -O $url
 tar -xf scripts.tar.gz
+
+rm -rf ./scripts.tar.gz
